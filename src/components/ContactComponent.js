005130439ea1bae60749a1e03081da2df31deede
kevinpaulJose <bkevin1999@gmail.com>
2020-06-05 22:11:58 +0530

    Reach Redux Form Revisited

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Breadcrumb, BreadcrumbItem, Button, Form, FormGroup, Input, Col, Label,Row, FormFeedback } from 'reactstrap';
+import { Breadcrumb, BreadcrumbItem, Button, Col, Label,Row } from 'reactstrap';
 import { Link } from 'react-router-dom';
-import { Control, LocalForm, Errors } from 'react-redux-form';
+import { Control, Form, Errors, actions } from 'react-redux-form';
 
 
 
@@ -19,6 +19,7 @@ class Contact extends Component {
     handleSubmit(value){
         console.log("Current State is: " +JSON.stringify(value));
         alert("Current State is: " +JSON.stringify(value));
+        this.props.resetFeedbackForm();
     }
     render() {  
         return(
@@ -64,7 +65,7 @@ class Contact extends Component {
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                    <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+                    <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
@@ -186,7 +187,7 @@ class Contact extends Component {
                                     </Button>
                                 </Col>
                             </Row>
-                        </LocalForm>
+                        </Form>
                     </div>
                 </div>
             </div>
